0bd051f669396893296bea70293a505368405d8d
alingse <alingse@foxmail.com>
2020-12-21 23:15:38 +0800

    fix

--- niaotuobang/merry/urls.py
+++ niaotuobang/merry/urls.py
@@ -19,7 +19,7 @@ router.register(r'tickets', views.TicketViewSet)
 
 urlpatterns = [
     path(r'api/whoami', views.CurrentUserView.as_view(), name='current_user'),
-    path(r'api/auth', include('rest_framework.urls', namespace='rest_framework')),
+    path(r'api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     path(r'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path(r'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path(r'api/', include(router.urls)),
